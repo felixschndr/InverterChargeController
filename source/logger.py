8f3581ffddfd3041a43c8a60843b037aa13b3b23
Felix Schneider <mail@fschneider.me>
2024-10-02 14:17:18 +0200

    Revert "Moved logging to file output"
    This reverts commit 92da539f

--- source/logger.py
+++ source/logger.py
@@ -1,30 +1,16 @@
 import logging
 import os
-from datetime import datetime
-from pathlib import Path
-
-from environment_variable_getter import EnvironmentVariableGetter
+import sys
 
 
 class LoggerMixin:
     def __init__(self):
-        log_directory_user_input = EnvironmentVariableGetter.get("LOGFILE_DIRECTORY")
-        path_to_source_folder = os.path.dirname(os.path.realpath(__file__))
-        path_to_repository = Path(path_to_source_folder).parent.absolute()
-        log_directory = os.path.join(  # noqa: FKA100
-            path_to_repository, log_directory_user_input
-        )
-        if not os.path.exists(log_directory):
-            os.makedirs(log_directory)
-        logfile_name = (
-            f"{log_directory}/{datetime.now().strftime('%Y-%m-%dT%H-%M-%S')}.log"
-        )
-
+        loglevel = os.environ.get("LOGLEVEL", "INFO").upper()  # noqa: FKA100
         logging.basicConfig(
             format="[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s",
             encoding="utf-8",
-            level=os.environ.get("LOGLEVEL", "INFO").upper(),  # noqa: FKA100
-            filename=logfile_name,
+            level=loglevel,
+            stream=sys.stdout,
         )
 
         logging.getLogger("gql.transport.aiohttp").setLevel(logging.WARNING)

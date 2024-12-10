6ed26dc9f5245515ece9a29e3232a7ed3470360d
Felix Schneider <fs@felix-schneider.org>
2024-12-10 13:20:21 +0100

    Add email error reporting feature
    Introduced a new script, `error_mailer.sh`, for sending error logs to an email address specified by `ERROR_MAIL_ADDRESS` in the configuration.

--- solar_forecast_logger.sh
+++ solar_forecast_logger.sh
@@ -14,7 +14,6 @@ source "${env_temp}"
 log_directory=${DIRECTORY_OF_LOGS:-logs/}
 logfile=${log_directory}/app${environment}.log
 
-
 ###### Find values of solar forecast ######
 start_string="The expected solar output of today is"
 temp_output_solar_forecast=$(mktemp)
@@ -26,11 +25,10 @@ done
 
 timestamp=$(head -n 1 "${temp_output_solar_forecast}" | awk '{print $1}' | tr -d '[]')
 date=$(date -d "${timestamp}" +%Y-%m-%d)
-solar_forecast_expected=$(grep "${start_string}" "${temp_output_solar_forecast}" | sed 's/^.* is //')
-solar_forecast_real=$(grep "The actual solar output of today was" "${temp_output_solar_forecast}" | sed 's/^.* was //')
+solar_forecast_expected=$(grep "${start_string}" "${temp_output_solar_forecast}" | sed 's/^.* is //' | sed 's/ Wh//')
+solar_forecast_real=$(grep "The actual solar output of today was" "${temp_output_solar_forecast}" | sed 's/^.* was //' | sed 's/ Wh//')
 
 echo -e "${date}\t${solar_forecast_expected}\t${solar_forecast_real}" >>"${log_directory}/solar_forecast_difference${environment}.log"
 
 ###### Cleanup ######
-
 rm "${temp_output_solar_forecast}" "${env_temp}"

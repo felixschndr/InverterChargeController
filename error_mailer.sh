fc6ba146e93399a50e4851e4020449b47787d24a
Felix Schneider <mail@fschneider.me>
2025-01-10 16:42:29 +0100

    error_mailer.sh also regonizes when the service is down

--- error_mailer.sh
+++ error_mailer.sh
@@ -4,6 +4,7 @@
 source .env
 log_directory=${DIRECTORY_OF_LOGS:-logs/}
 logfile=${log_directory}/app.log
+sender="chrctrl"
 
 if [[ -z ${ERROR_MAIL_ADDRESS} ]]; then
 	echo "The \"ERROR_MAIL_ADDRESS\" is not set!"
@@ -16,8 +17,13 @@ search_pattern="ERROR|WARNING|CRITICAL"
 temp_output=$(mktemp)
 
 grep -A10 -n "${current_date}" "${logfile}" | grep -A10 -E "${search_pattern}" > "${temp_output}"
-if [[ $(wc -l < "${temp_output}") != 0 ]]; then
-	 mail -r "InverterChargeController" -s "Fehler beim InverterChargeController" "${ERROR_MAIL_ADDRESS}" < "${temp_output}"
+if [[ $(wc -l < "${temp_output}") != 0 ]] || ! systemctl is-active --quiet ${sender}; then
+	if ! systemctl is-active --quiet ${sender}; then
+		subject="Der InverterChargeController laeuft nicht"
+	else
+		subject="Fehler beim InverterChargeController"
+	fi
+ 	mail -r "${sender}" -s "${subject}" "${ERROR_MAIL_ADDRESS}" < "${temp_output}"
 fi
 
 ###### Cleanup ######

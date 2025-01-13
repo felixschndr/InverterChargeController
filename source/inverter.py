a6686dd7b492af6e1c6ad01c872472d0dbf228ed
Felix Schneider <mail@fschneider.me>
2025-01-13 13:34:57 +0100

    Refactor inverter connection handling with property decorator
    Replaced manual connection logic with a `@property` to handle lazy initialization of the inverter device. Removed redundant `connect` method and simplified connection checks in various methods, improving code clarity and reducing duplication.

--- source/inverter.py
+++ source/inverter.py
@@ -3,6 +3,7 @@ import asyncio
 import goodwe
 from energy_classes import EnergyAmount
 from environment_variable_getter import EnvironmentVariableGetter
+from goodwe import inverter as GoodweInverter
 from goodwe.et import OperationMode
 from logger import LoggerMixin
 from sems_portal_api_handler import SemsPortalApiHandler
@@ -12,14 +13,35 @@ class Inverter(LoggerMixin):
     def __init__(self):
         super().__init__()
 
-        self.device = None
+        self._device = None
         self.hostname = EnvironmentVariableGetter.get("INVERTER_HOSTNAME")
 
         self.sems_portal_api_handler = SemsPortalApiHandler()
         self.battery_capacity = None
         self.update_battery_capacity()
 
+    @property
+    def device(self) -> GoodweInverter:
+        """
+        @property
+        Retrieves the inverter device instance. If not already connected, it establishes a connection to the inverter
+
+        Returns:
+            GoodweInverter: The instance of the connected inverter.
+        """
+        if self._device is None:
+            self.log.debug(f"Connecting to inverter on {self.hostname}...")
+            self._device = asyncio.run(goodwe.connect(self.hostname))
+            self.log.info("Successfully connected to inverter")
+
+        return self._device
+
     def update_battery_capacity(self) -> None:
+        """
+        Updates the battery capacity by retrieving the value using the SemsPortalApiHandler and comparing it
+        against the current stored capacity. Logs information about the current and updated battery
+        capacity if changes occur.
+        """
         battery_capacity = self.sems_portal_api_handler.get_battery_capacity()
 
         if self.battery_capacity is None:
@@ -31,30 +53,13 @@ class Inverter(LoggerMixin):
             self.log.info(f"The battery capacity was updated from {self.battery_capacity} to {battery_capacity}")
             self.battery_capacity = battery_capacity
 
-    def connect(self) -> None:
-        """
-        Connects to the inverter using the configured hostname.
-
-        Raises:
-            goodwe.exceptions.ConnectionException: If the connection to the inverter fails
-        """
-        self.log.debug(f"Connecting to inverter on {self.hostname}...")
-        self.device = asyncio.run(goodwe.connect(self.hostname))
-        self.log.info("Successfully connected to inverter")
-
     def get_operation_mode(self) -> OperationMode:
         """
         Gets the current operation mode of the device.
 
-        If the device is not already connected, it will attempt to connect before
-        fetching the operation mode.
-
         Returns:
             OperationMode: The current operation mode of the device.
         """
-        if self.device is None:
-            self.connect()
-
         self.log.debug("Getting current operation mode...")
         operation_mode = asyncio.run(self.device.get_operation_mode())
         self.log.info(f"The current Operation mode is {operation_mode.name}")
@@ -62,15 +67,14 @@ class Inverter(LoggerMixin):
 
     def set_operation_mode(self, mode: OperationMode) -> None:
         """
+        Sets the current operation mode of the device.
+
         Args:
             mode: The desired operation mode to be set for the inverter.
 
         Raises:
             RuntimeError: If the operation mode could not be set successfully.
         """
-        if self.device is None:
-            self.connect()
-
         self.log.debug(f"Setting new operation mode: {mode.name}...")
         asyncio.run(self.device.set_operation_mode(mode))
 
@@ -123,9 +127,6 @@ class Inverter(LoggerMixin):
         Returns:
             int: The current state of charge in percentage.
         """
-        if self.device is None:
-            self.connect()
-
         self.log.debug("Getting current state of charge...")
         runtime_data = asyncio.run(self.device.read_runtime_data())
         state_of_charge = runtime_data["battery_soc"]

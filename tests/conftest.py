b743a981aa90e7b3ec7d343cf26ef17d858b502c
Felix Schneider <mail@fschneider.me>
2026-09-22 18:03:23 +0200

    fix: tests in pipeline

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,25 @@
+import os
+
 import pytest
 
-from source import environment_variable_getter
+dummy = "dummy"
+
+for name, value in {
+    "INVERTER_BATTERY_CAPACITY": "10000",
+    "INVERTER_HOSTNAME": "192.0.2.1",
+    "TIBBER_API_TOKEN": dummy,
+    "SEMSPORTAL_USERNAME": dummy,
+    "SEMSPORTAL_PASSWORD": dummy,
+    "SEMSPORTAL_POWERSTATION_ID": dummy,
+    "SOLCAST_API_KEY": dummy,
+    "ROOFTOP_ID_1": dummy,
+    "LATITUDE": "48.1",
+    "LONGITUDE": "11.5",
+    "INFLUXDB_TOKEN": dummy,
+}.items():
+    os.environ.setdefault(name, value)
+
+from source import environment_variable_getter  # noqa: E402
 
 
 @pytest.fixture
